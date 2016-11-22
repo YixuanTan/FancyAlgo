861c2e3a940a2fdaa27e526e021d6cb2512ef295
Yixuan <tanyx1990@gmail.com>
2016-11-21 21:16:22 -0500

    Initial Commit

---
+++ FancyAlgo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FancyAlgo
+//
+//  Created by Yixuan Tan on 11/21/16.
+//  Copyright © 2016 Yixuan Tan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
